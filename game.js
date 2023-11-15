70e3da6f52298f6532ef83457dc3458545431b9b
Peyton <65147510+Erkite@users.noreply.github.com>
2023-11-15 11:31:12 -0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,16 @@
+function madLib()
+{
+   let name =prompt("A person's name");
+   let exclamation =prompt("An exclamation");
+   let pluralNoun =prompt("A plural noun");
+   let city =prompt("The name of a city");
+   let food =prompt("Something good to eat");
+   let emotion =prompt("An emotion");
+   let randomPhrase =prompt("Some random phrase");
+
+   console.log("I love "+pluralNoun+" and "+food+" said "+name+"! "+exclamation+", I've made it to "+city);
+   console.log("I'm still "+emotion);
+   console.log("I'll proclaim "+randomPhrase+"!");
+
+   // Your solution goes here 
+}
